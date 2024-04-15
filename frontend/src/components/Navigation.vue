0008a27d48d54b21afa7b38babff9cca5c3209aa
Artem Logvinov <artemartem330@yandex.ru>
2024-04-16 00:04:18 +0300

    Navigation created & routes changed

---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <router-link to="/"
+      ><img src="../assets/pizza-logo.png" alt="" width="100px"
+    /></router-link>
+    |
+    <router-link to="/cart">Корзина</router-link>
+    <router-link to="/login">Войти</router-link>
+    <router-link to="/register">Регистрация</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
