f3ec1bb7b9403057e9b2cbcc05105f41ef0c4c6f
Mark Solonchev <solonchev32@gmail.com>
2024-04-16 22:24:09 +0300

    add CORS support to controller

--- backend/src/main/java/ru/solonchev/backend/controller/BackendController.java
+++ backend/src/main/java/ru/solonchev/backend/controller/BackendController.java
@@ -14,6 +14,7 @@ import ru.solonchev.backend.service.UserService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/pizza-store")
 @RequiredArgsConstructor
 @Slf4j
