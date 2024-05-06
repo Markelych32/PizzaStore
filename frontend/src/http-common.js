53dcc70e7f27b95053aae57b89d8e6c1fa6dbfe9
Artem Logvinov <artemartem330@yandex.ru>
2024-05-06 16:49:15 +0300

    cors

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const AXIOS = axios.create({
   baseURL: `http://localhost:8081`,
   headers: {
-    "Access-Control-Allow-Origin": "http://localhost:9090",
+    "Access-Control-Allow-Origin":
+      "https://pizza-store-backend-614r.onrender.com:9090",
     "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
     "Access-Control-Allow-Headers":
       "Content-Type, Authorization, X-Requested-With",
